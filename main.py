9e19124b3410e7465b1b9708562bb0350f297ede
Kartikeya Tripathi <96724863+ktwillcode@users.noreply.github.com>
2024-10-31 01:43:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+from interview_simulation import InterviewSimulation
+
+def main():
+    job_titles = [
+        "Marketing Associate",
+        "Business Development Representative",
+        "Product Manager",
+        "Customer Success Representative",
+        "Data Analyst",
+        "AI Engineer" #add more if you want
+    ]
+    
+    job_title = job_titles[5]  # Change index to try different positions
+    simulation = InterviewSimulation(job_title)
+    simulation.conduct_interviews()
+
+if __name__ == "__main__":
+    main()
